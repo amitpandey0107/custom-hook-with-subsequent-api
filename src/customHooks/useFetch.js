05ea7e0b471fd044f006a58cfc19dc7e1a967751
Amit <amitpandey0107@gmail.com>
2025-02-11 20:55:38 +0530

    Simplied custom hook

--- src/customHooks/useFetch.js
+++ src/customHooks/useFetch.js
@@ -1,24 +1,30 @@
 import { useState, useEffect } from "react";
 export const useFetch = (url) => {
-    const [data, setData] = useState(null);
-    const [isPending, setIsPending] = useState(false);
+    const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(false)
     const [error, setError] = useState(null);
-    useEffect(() => {
-        const fetchData = async () => {
-            setIsPending(true);
-            try {
-                const response = await fetch(url);
-                if (!response.ok) throw new Error(response.statusText);
-                const json = await response.json();
-                setIsPending(false);
-                setData(json);
-                setError(null);
-            } catch (error) {
-                setError(`${error} Could not Fetch Data `);
-                setIsPending(false);
-            }
-        };
-        fetchData();
-    }, [url]);
-    return { data, isPending, error };
-};
+
+    const fetchData = async(url) =>{
+        try {
+            setIsPending(true)
+            await fetch(url)
+            .then((res)=>res.json())
+            .then((res)=>{
+                if(res){
+                    setData(res)
+                    setIsPending(false)
+                    setError(null)
+                }
+            })
+        } catch(err) {
+            setError(err);
+            setIsPending(false)
+        }
+    }
+
+    useEffect(()=>{
+        fetchData(url)
+    }, [url])
+
+    return {data, isPending, error}
+}
